ffbdfbf8a6eb116496084aaf6dfe7cb323c1b1ed
[dyad] <git@dyad.sh>
2025-07-16 11:47:30 +0700

    [dyad] wrote 1 file(s)

--- src/components/lesson/LessonDetails.tsx
+++ src/components/lesson/LessonDetails.tsx
@@ -31,16 +31,6 @@ const LessonDetails: React.FC<LessonDetailsProps> = ({
           Thêm ghi chú tại 00:00
         </Button>
       </div>
-
-      <div className="text-gray-700 mt-6">
-        <p className="mb-3">Tham gia các cộng đồng để cùng học hỏi, chia sẻ và "thăm thính" xem SSStudy sắp có gì mới nhé!</p>
-        <ul className="list-disc list-inside space-y-2">
-          <li><a href="https://www.facebook.com/f8nofficial" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Fanpage: https://www.facebook.com/f8nofficial</a></li>
-          <li><a href="https://www.facebook.com/groups/649972919142215" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Group: https://www.facebook.com/groups/649972919142215</a></li>
-          <li><a href="https://www.youtube.com/F8VNOfficial" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Youtube: https://www.youtube.com/F8VNOfficial</a></li>
-          <li><a href="https://www.facebook.com/sondnf8" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">Sơn Đặng: https://www.facebook.com/sondnf8</a></li>
-        </ul>
-      </div>
     </div>
   );
 };
