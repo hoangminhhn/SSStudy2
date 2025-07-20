6ff602f9236891d48bf0e0099299efee2289d867
[dyad] <hoangminhphungbk@gmail.com>
2025-07-20 22:53:18 +0700

    [dyad] Chỉ đặt màu văn bản BreadcrumbNav thành đen cho trang V2 - wrote 1 file(s)

--- src/pages/CourseDetailPageV2.tsx
+++ src/pages/CourseDetailPageV2.tsx
@@ -63,7 +63,7 @@ const CourseDetailPageV2 = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col v2-theme">
       <Header />
-      <BreadcrumbNav courseTitle={course.title} bgColor="black" variant="v2" /> {/* Added variant="v2" */}
+      <BreadcrumbNav courseTitle={course.title} variant="v2" /> {/* Removed bgColor="black" */}
       <main className="flex-grow container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2">
