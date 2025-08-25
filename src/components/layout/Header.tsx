6054a0423e696cfb9e7ea54514fa2bd8ca70bfeb
[dyad] <hoangminhphungbk@gmail.com>
2025-08-25 16:14:21 +0700

    [dyad] Thêm submenu 'Khóa học' dẫn tới V2 - wrote 1 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search, Bell, ShoppingCart, User } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 
 const Header = () => {
@@ -15,13 +20,37 @@ const Header = () => {
         <Link to="/" className="text-2xl font-bold text-blue-600">
           S
         </Link>
-        <nav className="hidden md:flex space-x-6">
+        <nav className="hidden md:flex space-x-6 items-center">
           <Link to="#" className="text-gray-700 hover:text-blue-600 font-medium">
             Giới thiệu
           </Link>
-          <Link to="/courses" className="text-gray-700 hover:text-blue-600 font-medium"> {/* Updated link */}
-            Khóa học
-          </Link>
+
+          {/* Khóa học with dropdown submenu */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="text-gray-700 hover:text-blue-600 font-medium">
+                Khóa học
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start" className="min-w-[180px]">
+              <DropdownMenuItem asChild>
+                <Link to="/courses" className="block w-full">
+                  Khóa học (phiên bản cũ)
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link to="/courses-v2" className="block w-full">
+                  Khóa học V2
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link to="/courses-v3" className="block w-full">
+                  Khóa học V3
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           <Link to="#" className="text-gray-700 hover:text-blue-600 font-medium">
             Sách
           </Link>
