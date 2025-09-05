e8e1f86b0ea3e0b44a5f834d0039f9d8b28fcc7e
[dyad] <hoangminhphungbk@gmail.com>
2025-09-05 21:46:02 +0700

    Reverted all changes back to version a48ac330b5c6f70416ace1703c3ca498808c7819

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,6 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import HeroSlider from "@/components/home/HeroSlider";
 import StatsSection from "@/components/home/StatsSection";
-import ProgramsSection from "@/components/home/ProgramsSection";
 
 const Index = () => {
   return (
@@ -15,7 +14,6 @@ const Index = () => {
       <main className="flex-grow">
         <HeroSlider />
         <StatsSection />
-        <ProgramsSection />
       </main>
       <Footer />
     </div>
