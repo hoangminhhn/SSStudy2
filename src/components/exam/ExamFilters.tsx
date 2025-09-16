f53682f696b2058e609f4f8416598dfd7169ca97
[dyad] <hoangminhphungbk@gmail.com>
2025-09-16 10:49:56 +0700

    [dyad] Fix Input import in ExamFilters - wrote 1 file(s)

--- src/components/exam/ExamFilters.tsx
+++ src/components/exam/ExamFilters.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Input from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 import { ChevronDown, ChevronUp, X } from "lucide-react";
 
 type Pill = {
